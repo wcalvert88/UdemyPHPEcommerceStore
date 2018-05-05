92e6d86e7ce01b4c2b0339260ded659a04af5047
wcalvert88 <wcalvert88@gmail.com>
2018-05-05 17:00:38 -0400

    got side nav dynamic from database

--- public/index.php
+++ public/index.php
@@ -10,14 +10,14 @@ include(TEMPLATE_FRONT . DS . "header.php");
         <div class="row">
 
             <!-- Categories here -->
-            <?php include("TEMPLATE_FRONT" . DS . "side_nav.php"); ?>
+            <?php include(TEMPLATE_FRONT . DS . "side_nav.php"); ?>
             <div class="col-md-9">
 
                 <div class="row carousel-holder">
 
                     <div class="col-md-12">
                         <!-- Carousel -->
-                        <?php include("TEMPLATE_FRONT" . DS . "slider.php"); ?>
+                        <?php include(TEMPLATE_FRONT . DS . "slider.php"); ?>
                         
                     </div>
 
