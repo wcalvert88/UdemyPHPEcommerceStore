9599d21bca0f282bc0c4c7a2fb0315da4584882d
wcalvert88 <wcalvert88@gmail.com>
2018-05-05 16:53:36 -0400

    created the categories table

--- resources/templates/front/side_nav.php
+++ resources/templates/front/side_nav.php
@@ -1,6 +1,11 @@
 <div class="col-md-3">
     <p class="lead">Shop Name</p>
     <div class="list-group">
+    <?php 
+    
+    
+    
+    ?>
         <a href="category.html" class="list-group-item">Category 1</a>
         <a href="#" class="list-group-item">Category 2</a>
         <a href="#" class="list-group-item">Category 3</a>
