8f3d0758cc3c989f45a3d5eab991b81d7d0e7b7d
wcalvert88 <wcalvert88@gmail.com>
2018-05-05 19:23:18 -0400

    created helper functions

--- resources/functions.php
+++ resources/functions.php
@@ -1,4 +1,27 @@
 <?php 
 
+function redirect($location) {
+    header("Location: $location");
+}
+
+function query($sql) {
+    global $connection;
+    return mysqli_query($connection, $sql);
+}
+
+function confirm($result) {
+    if (!$result) {
+        die("QUERY FAILED" . mysqli_error($connection));
+    }
+}
+
+function escape($string) {
+    global $connection;
+    return mysqli_real_escape_string($connection, $string);
+}
+
+function fetch_array($result) {
+    return mysqli_fetch_array($result);
+}
 
 ?>
