03526ee540a8b9df5277306cb1445c3ab3d9e88c
wcalvert88 <wcalvert88@gmail.com>
2018-05-06 12:04:52 -0400

    finished item.php page and added short_description column to db

--- public/item.php
+++ public/item.php
@@ -23,7 +23,7 @@ include(TEMPLATE_FRONT . DS . "header.php");
 <div class="row">
 
     <div class="col-md-7">
-       <img class="img-responsive" src="http://placehold.it/700x600" alt="">
+       <img class="img-responsive" src="<?php echo $row['product_image'];?>" alt="">
 
     </div>
 
@@ -49,7 +49,7 @@ include(TEMPLATE_FRONT . DS . "header.php");
         </p>
     </div>
           
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+        <?php echo $row['short_description']; ?>
 
    
     <form action="">
@@ -90,7 +90,7 @@ include(TEMPLATE_FRONT . DS . "header.php");
 
 <p></p>
            
-    <?php echo $row['product_description']; ?>
+    <p><?php echo $row['product_description']; ?></p>
     </div>
     <div role="tabpanel" class="tab-pane" id="profile">
 
