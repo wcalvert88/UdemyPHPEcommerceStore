edeaa97365eb1107d6771fda8b6fe187e98511b2
wcalvert88 <wcalvert88@gmail.com>
2018-05-06 14:26:46 -0400

    created functionality for removing and deleting items from checkout

--- public/cart.php
+++ public/cart.php
@@ -19,5 +19,18 @@ if(isset($_GET['add'])) {
     // redirect("index.php");
 }
 
+if(isset($_GET['remove'])) {
+    $_SESSION['product_' . $_GET['remove']]--;
+    if($_SESSION['product_' . $_GET['remove']] < 1) {
+        redirect("checkout.php");
+    } else {
+        redirect("checkout.php");
+    }
+}
+
+if(isset($_GET['delete'])) {
+    $_SESSION['product_' . $_GET['delete']] = 0;
+    redirect("checkout.php");
+}
 ?>
 
