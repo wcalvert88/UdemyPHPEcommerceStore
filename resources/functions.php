ef2abf441066b1ea7869d0c6bd797dbbab72e86e
wcalvert88 <wcalvert88@gmail.com>
2018-05-05 20:20:09 -0400

    edited the product links

--- resources/functions.php
+++ resources/functions.php
@@ -32,14 +32,14 @@ function get_products() {
     while($row = fetch_array($query)) {
         $product = <<<DELIMETER
         <div class="col-sm-4 col-lg-4 col-md-4">
-            <div class="thumbnail">
-                <img src="{$row['product_image']}" alt="">
+            <a href="item.php?id={$row['product_id']}"><div class="thumbnail">
+                <img src="{$row['product_image']}" alt=""></a>
                 <div class="caption">
-                    <h4 class="pull-right">{$row['product_price']}</h4>
+                    <h4 class="pull-right">&#36;{$row['product_price']}</h4>
                     <h4><a href="product.html">{$row['product_title']}</a>
                     </h4>
                     <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
-                    <a class="btn btn-primary" target="_blank" href="http://maxoffsky.com/code-blog/laravel-shop-tutorial-1-building-a-review-system/">View Tutorial</a>
+                    <a class="btn btn-primary" target="_blank" href="item.php?id={$row['product_id']}">Add to Cart</a>
                 </div>
                 <!-- <div class="ratings">
                     <p class="pull-right">15 reviews</p>
