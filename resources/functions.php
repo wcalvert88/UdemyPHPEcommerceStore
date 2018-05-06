910da9b36a68f9e37720edd8521cc46f50e5a19e
wcalvert88 <wcalvert88@gmail.com>
2018-05-05 20:03:23 -0400

    used delimeter to display products

--- resources/functions.php
+++ resources/functions.php
@@ -30,8 +30,32 @@ function get_products() {
     $query = query("SELECT * FROM products");
     confirm($query);
     while($row = fetch_array($query)) {
-        echo $row['product_price'];
+        $product = <<<DELIMETER
+        <div class="col-sm-4 col-lg-4 col-md-4">
+            <div class="thumbnail">
+                <img src="{$row['product_image']}" alt="">
+                <div class="caption">
+                    <h4 class="pull-right">{$row['product_price']}</h4>
+                    <h4><a href="product.html">{$row['product_title']}</a>
+                    </h4>
+                    <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
+                    <a class="btn btn-primary" target="_blank" href="http://maxoffsky.com/code-blog/laravel-shop-tutorial-1-building-a-review-system/">View Tutorial</a>
+                </div>
+                <!-- <div class="ratings">
+                    <p class="pull-right">15 reviews</p>
+                    <p>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                    </p>
+                </div> -->
+            </div>
+        </div>
 
+DELIMETER;
+    echo $product;
     }
 }
 
