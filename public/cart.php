998e44b80576a8039737bb57ff08e00fe2bc5d84
wcalvert88 <wcalvert88@gmail.com>
2018-05-06 14:16:15 -0400

    made checkout pull items from database and check to make sure we have enough items in stock

--- public/cart.php
+++ public/cart.php
@@ -2,8 +2,20 @@
 require_once("../resources/config.php");
 
 if(isset($_GET['add'])) {
-    $_SESSION['product_' . $_GET['add']] += 1;
-    redirect("index.php");
+
+    $query = query("SELECT * FROM products WHERE product_id = " . escape($_GET['add']) . " ");
+    confirm($query);
+
+    while ($row = fetch_array($query)) {
+        if($row['product_quantity'] != $_SESSION['product_' . $_GET['add']]) {
+            $_SESSION['product_' . $_GET['add']] += 1;
+        } else {
+            set_message("We only have " . $row['product_quantity'] . " available");
+            redirect("checkout.php");
+        }
+    }
+    // $_SESSION['product_' . $_GET['add']] += 1;
+    // redirect("index.php");
 }
 
 ?>
